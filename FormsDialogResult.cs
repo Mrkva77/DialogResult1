046d980495a19038b5423b8597231799d3293706
Mrkva77 <156481047+Mrkva77@users.noreply.github.com>
2024-10-09 08:35:38 +0200

    Add files via upload

---
+++ FormsDialogResult.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DialogResoult2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOtvori_Click(object sender, EventArgs e)
+        {
+            string naslov = "Naslovna linija Dijaloga za poruke";
+            string poruka = "Odaberite Yes / No / Cancel";
+
+            MessageBoxButtons buttons = MessageBoxButtons.YesNoCancel;
+            MessageBoxIcon ikona = MessageBoxIcon.Information;
+
+            // MessageBox.Show() vraća vrijednost tipa DialogResult
+
+            // tu vrijednost dodjeljujemo varijabli rez
+
+            DialogResult rez = MessageBox.Show(poruka, naslov, buttons, ikona);
+
+            switch (rez)
+
+            {
+
+                case DialogResult.Yes:
+
+                    textBoxRezultat.Text = "Izabrali ste dugme YES";
+
+                    break;
+
+                case DialogResult.No:
+
+                    textBoxRezultat.Text = "Izabrali ste dugme NO";
+
+                    break;
+
+                case DialogResult.Cancel:
+
+                    textBoxRezultat.Text = "Izabrali ste dugme Cancel";
+
+                    break;
+
+            }
+        }
+    }
+}
